4da2d42de462fc7874a958c75aef962fccee7eb6
MajonesTube <martine.vold@hotmail.com>
2025-03-25 19:53:18 +0100

    Design på hjemmesiden delvis implementert. Testliste med tall.

--- app/src/main/java/com/example/oblig3/data/ArtUiState.kt
+++ app/src/main/java/com/example/oblig3/data/ArtUiState.kt
@@ -3,5 +3,6 @@ package com.example.oblig3.data
 data class ArtUiState(
     val pictures: List<Photo> = listOf(),
     val frames: List<FrameType> = listOf(),
+    val numberOfPicturesChosen: Int = 0,
     val price: String = ""
 )
--- app/src/main/java/com/example/oblig3/ui/MainScreen.kt
+++ app/src/main/java/com/example/oblig3/ui/MainScreen.kt
@@ -5,30 +5,43 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
 import com.example.oblig3.R
 import com.example.oblig3.data.Photo
 import com.example.oblig3.ui.theme.bodyFontFamily
 
 @Composable
 fun MainScreen(
-    //photos: List<Photo>,
     onNextButtonClicked: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val testPhotoList: List<String> = listOf("1", "2", "3",
+        "4", "5", "6", "7", "8",
+        "9", "10", "11,", "12", "14", "4", "5", ",", ",", ",", ",", ",",
+        "9", "10", "11,", "12", "14", "4", "5", ",", ",", ",", ",", ",",
+        "9", "10", "11,", "12", "14", "4", "5", ",", ",", ",", ",", ",")
+
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.SpaceBetween
@@ -36,8 +49,10 @@ fun MainScreen(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = dimensionResource(R.dimen.padding_small),
-                    end = dimensionResource(R.dimen.padding_small) ),
+                .padding(
+                    start = dimensionResource(R.dimen.padding_small),
+                    end = dimensionResource(R.dimen.padding_small)
+                ),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
@@ -53,15 +68,52 @@ fun MainScreen(
                     onClick = { /*TODO*/ },
                     modifier = Modifier.weight(1f)
                 ) {
-                    Text("Kunstner")
+                    Text(
+                        stringResource(R.string.kunstner)
+                    )
                 }
                 Spacer(modifier.width(dimensionResource(R.dimen.padding_small)))
                 Button(
                     onClick = { /*TODO*/ },
                     modifier = Modifier.weight(1f)
                 ) {
-                    Text("Kategori")
+                    Text(
+                        text = stringResource(R.string.kategori)
+                    )
                 }
+
+            }
+            Text(
+                text = stringResource(R.string.antall_bilder_valgt, /*TODO*/),
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = stringResource(R.string.totalpris, /*TODO*/),
+                fontWeight = FontWeight.Bold
+            )
+            if (testPhotoList.isNotEmpty()) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(1),
+                    modifier = Modifier
+                        .heightIn(min = 0.dp, max = LocalConfiguration.current.screenHeightDp.dp * 0.55f)
+                ) {
+                    items(testPhotoList) { item ->
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = item)
+                        }
+                    }
+                }
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth(0.9f)
+            ) {
+                Text(
+                    text = stringResource(R.string.til_betaling)
+                )
             }
         }
 
