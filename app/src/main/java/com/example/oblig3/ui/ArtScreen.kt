3a00594d9cc6d775ce8dfb2ca239a3d34b6e56a8
MajonesTube <martine.vold@hotmail.com>
2025-03-25 19:16:02 +0100

    Starter på main side. Lagt til overskrift og knapper

--- app/src/main/java/com/example/oblig3/ui/ArtScreen.kt
+++ app/src/main/java/com/example/oblig3/ui/ArtScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/example/oblig3/ui/MainScreen.kt
+++ app/src/main/java/com/example/oblig3/ui/MainScreen.kt
@@ -1,9 +1,27 @@
 package com.example.oblig3.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import com.example.oblig3.R
 import com.example.oblig3.data.Photo
+import com.example.oblig3.ui.theme.bodyFontFamily
 
 @Composable
 fun MainScreen(
@@ -11,7 +29,41 @@ fun MainScreen(
     onNextButtonClicked: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Text(
-        text = "HEI!!!"
-    )
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = dimensionResource(R.dimen.padding_small),
+                    end = dimensionResource(R.dimen.padding_small) ),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(
+                modifier = Modifier.height(dimensionResource(R.dimen.padding_small))
+            )
+            Text(
+                text = stringResource(R.string.velg_bilde_basert_paa),
+                fontWeight = FontWeight.Bold
+            )
+            Row {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text("Kunstner")
+                }
+                Spacer(modifier.width(dimensionResource(R.dimen.padding_small)))
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text("Kategori")
+                }
+            }
+        }
+
+    }
 }
