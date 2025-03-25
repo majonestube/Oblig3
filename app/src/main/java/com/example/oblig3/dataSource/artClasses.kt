60019e20aaa5af12f0df5c994ecfc3bacb8af786
MajonesTube <martine.vold@hotmail.com>
2025-03-25 13:28:27 +0100

    updated oblig 3

---
+++ app/src/main/java/com/example/oblig3/dataSource/artClasses.kt
@@ -0,0 +1,47 @@
+package com.example.oblig3.dataSource
+
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+
+data class Photo(
+    /** Unik ID til bildet **/
+    val id: Long,
+    val title: String = "",
+    @DrawableRes
+    val imageResId: Int,
+    val artist: Artist,
+    val category: Category,
+    val price: Float = 0.0f
+)
+
+data class Artist(
+    val id: Long,
+    val name: String = "",
+    val familyName: String = ""
+)
+
+data class SelectedPhoto(
+    val photoId: Long,
+    val frameType: FrameType,
+    val frameWidth: Int,
+    val photoSize: PhotoSize,
+    val photoPrice: Float = 0.0f,
+)
+
+enum class Category {
+    NATURE(),
+    FOOD(),
+    SPORT()
+}
+
+enum class FrameType(val extraPrice: Float, val color: Color = Color.Yellow) {
+    WOOD(0f, color = Color.Yellow),
+    METAL(100f, color = Color.Blue),
+    PLASTIC(30f, color = Color.Green)
+}
+
+enum class PhotoSize(val extraPrice: Float, val size: Int = 170) {
+    SMALL(0f, size=170),
+    MEDIUM(130f, size=200),
+    LARGE(230f, size=250)
+}
