aa513e11f97dfe4d6a9d0bb24a02e4a0f021c388
MajonesTube <martine.vold@hotmail.com>
2025-03-25 18:08:43 +0100

    Implementert tema

--- app/src/main/java/com/example/oblig3/MainActivity.kt
+++ app/src/main/java/com/example/oblig3/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.oblig3.ui.theme.Oblig3Theme
 
@@ -18,11 +23,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Oblig3Theme {
+            Oblig3Theme(dynamicColor = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    ArtComposable(
+                        onClick = {/*TODO*/},
+                        modifier = Modifier.padding(innerPadding),
+
                     )
                 }
             }
@@ -31,17 +37,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArtComposable(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Row {
+        Text(
+            text = "Kunsthandler"
+        )
+        Button(
+            onClick = onClick
+        ){
+            Text("Test")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Oblig3Theme {
-        Greeting("Android")
+    Oblig3Theme(dynamicColor = false)  {
+        ArtComposable(
+            onClick = {/*TODO*/},
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
